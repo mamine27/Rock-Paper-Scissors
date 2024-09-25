a3cc076d1620970b37646847b74a250ca3095998
mamine27 <aminmamine620@gmail.com>
2024-09-25 16:08:36 +0300

    fixed gamelocgic error

--- script/script.js
+++ script/script.js
@@ -1,38 +1,52 @@
 var Humanscore = 0
 var Compscore = 0
+var correct
 var round = prompt("How many rounds?")
 
+// dictionary to communicate between the comp and user
+const myDictionary = {
+  "Scissors": 1,
+  "Paper": 2,
+  "Rock": 3 ,
+  1 :"Scissors",
+  2 :"Paper" ,
+  3 :"Rock"
+};
 
 function getcomputerchoice(){
     return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 }
 function getHumanchoice(){
     let s = prompt("Pick")
-    return s
+    correct = s[0].toUpperCase() + s.substring(1).toLowerCase() 
+    return myDictionary[correct]
 }
 function res(){
     if (Humanscore > Compscore){
         console.log("Nah , you won")
     }
-    else{
+    else if (Humanscore < Compscore){
         console.log("you were cooked")
+    }
+    else{
+        console.log("this was epic but it's tie")
     }
 }
 
 function Engine(){
-    let A = getcomputerchoice()
-    let B = getHumanchoice()
+    let A = getHumanchoice()
+    let B = getcomputerchoice()
     if ((A == 1 && B == 2) || (A == 2 && B == 3) || (A == 3 && B == 1)){
-        console.log("you win")
+        console.log(`${myDictionary[A]} beat ${myDictionary[B]} , you win this round`)
         console.log(++Humanscore , Compscore)
     }
     else if((A == 1 && B == 3) || (A == 2 && B == 1) || (A == 3 && B == 2)){
-        console.log("you lost")
+        console.log(`${myDictionary[A]} doesn't beat ${myDictionary[B]} , you lose this round`)
         console.log(Humanscore , ++Compscore)
 
     }
     else{
-        console.log("Tie")
+        console.log("this round was  a Tie")
         console.log(Humanscore,Compscore)
     }
     
@@ -41,4 +55,5 @@ function Engine(){
 for (let i = 0 ; i < round ; i++){
     Engine()
 }
+res()
 
